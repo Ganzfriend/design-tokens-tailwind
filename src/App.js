61c444e5ef15baf578c5af1bb64b67487942a529
Ganzfriend <melissaganzfried@gmail.com>
2022-10-06 16:26:14 -0600

    initial commit; tailwind classes are applying correctly from transformed and formatted tokens

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="bg-Primary-light">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="font-bold text-Secondary-dark">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
+const tokens = require("./tokens/tokens");
+const { colors } = tokens || {};
+
+const flattenObj = (key, obj) => {
+  if (obj === undefined) return;
+
+  const getEntries = () =>
+    Object.fromEntries(Object.entries(obj)?.map(([k, v]) => flattenObj(k, v)));
+
+  return typeof obj?.value === "string"
+    ? [key, obj.value]
+    : [key, getEntries()];
+};
+
+const formattedColors = Object.fromEntries(
+  Object.entries(colors)?.map(([k, v]) => {
+    const isStringOrNum = typeof v === "string" || typeof v === "number";
+    return isStringOrNum ? [k, v] : flattenObj(k, v);
+  })
+);
+
 module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: formattedColors,
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
