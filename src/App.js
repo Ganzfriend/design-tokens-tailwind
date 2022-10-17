39360332145215793efce539cb09558e2adc4698
Ganzfriend <melissaganzfried@gmail.com>
2022-10-17 12:55:47 -0700

    Add colors from theme to test

--- src/App.js
+++ src/App.js
@@ -21,23 +21,47 @@ function App() {
           Learn React
         </a>
       </header>
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          bgcolor: "primary.main",
+        }}
+      >
         <Typography variant="h1-regular" color="text.secondary">
           H1 regular
         </Typography>
         <Typography variant="h1-italic">H1 italic</Typography>
         <Typography variant="h1-bold">H1 bold</Typography>
       </Box>
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          bgcolor: "secondary.light",
+        }}
+      >
         <Typography variant="h6-regular">H6 regular</Typography>
         <Typography variant="h6-italic">H6 italic</Typography>
         <Typography variant="h6-bold">H6 bold</Typography>
       </Box>
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          bgcolor: "grey.main",
+        }}
+      >
         <Typography variant="body1">Body 1</Typography>
         <Typography variant="body2">Body 2</Typography>
       </Box>
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          bgcolor: "button.selected",
+        }}
+      >
         <Typography variant="subtitle1-regular">Subtitle 1 Regular</Typography>
         <Typography variant="subtitle1-semiBold">
           Subtitle 1 SemiBold
@@ -47,13 +71,32 @@ function App() {
           Subtitle 2 SemiBold
         </Typography>
       </Box>
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          bgcolor: "warning.main",
+        }}
+      >
         <Typography variant="caption">Caption</Typography>
       </Box>
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          bgcolor: "success.light",
+        }}
+      >
         <Typography variant="button">Button</Typography>
       </Box>
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          bgcolor: "background.black.primary",
+          color: "type.white.tertiary",
+        }}
+      >
         <Typography variant="overline">Overline</Typography>
       </Box>
     </div>
